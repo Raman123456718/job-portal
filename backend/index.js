948e54f88fc98c42eea58977f1ce4d05cb693ee2
Raman Kumar <ramankumar2882006@gmail.com>
2026-06-03 12:20:43 +0530

    Fix production CORS

--- backend/index.js
+++ backend/index.js
@@ -8,33 +8,40 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config({});
+dotenv.config();
 
 const app = express();
 
-// middleware
+// Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-const corsOptions = {
-    origin:'http://localhost:5173',
-    credentials:true
-}
 
-app.use(cors(corsOptions));
+// CORS Configuration
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://job-portal-two-dun.vercel.app",
+    ],
+    credentials: true,
+  })
+);
 
 const PORT = process.env.PORT || 3000;
 
-
-// api's
+// Routes
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Job Portal Backend Running Successfully 🚀");
+});
 
-
-app.listen(PORT,()=>{
-    connectDB();
-    console.log(`Server running at port ${PORT}`);
-})
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server running at port ${PORT}`);
+});
